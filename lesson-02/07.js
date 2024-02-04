c362c83985d19f50dcd59f9b4a83abaed683ac88
ZhenyaHoholko <zhenya.hoholko@gmail.com>
2024-02-04 22:55:37 +0300

    Fixed

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,14 +1,16 @@
-let passportMarried={
+let passportMarried = {
     name: "Petr",
     surname: "Petrov",
     address: {
         country: "USA",
         city: "Bobryisk"
     }
-}
-let passportMarried2=JSON.parse(JSON.stringify(passportMarried));
-const {address, ...rest}= passportMarried2;
-const {surname, ...restWithoutSurname}= rest;
-passportMarried2 = {surname, ...restWithoutSurname, married: true, address};
+};
+
+let passportMarried2 = JSON.parse(JSON.stringify(passportMarried));
+const { address, ...rest } = passportMarried2;
+const { surname, ...restWithoutSurname } = rest;
+passportMarried2 = { surname, ...restWithoutSurname, married: true, address };
+
 console.log(passportMarried);
 console.log(passportMarried2);
