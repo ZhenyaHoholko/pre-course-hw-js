578fdc092de94bbb2bbecd5d49eb86a96fbb1493
ZhenyaHoholko <zhenya.hoholko@gmail.com>
2024-02-29 23:53:57 +0300

    Update

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,15 +1,3 @@
-const calls = spy.getCalls();
-if (calls.length > 20) {
-    const task2ConsoleCall = calls[20].lastArg;
-    if (task2ConsoleCall) {
-        cy.expect(task2ConsoleCall).to.equal(withOrangeAnswer);
-    } else {
-        console.error('task2ConsoleCall is undefined');
-    }
-} else {
-    console.error('Index 20 is out of bounds for calls array');
-}
-
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 
 if (words.includes('апельсин')) {
