f2d6fcf1c170362b0e46e8331817b7040590de08
ZhenyaHoholko <zhenya.hoholko@gmail.com>
2024-03-03 23:38:47 +0300

    Update

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,4 +1,3 @@
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
-
 const slicedFruits = fruits.slice(1, -2);
 console.log(slicedFruits);
