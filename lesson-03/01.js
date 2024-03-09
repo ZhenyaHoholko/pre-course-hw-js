4bbd7d9c624695275febe28a9665b61669f72cfa
ZhenyaHoholko <zhenya.hoholko@gmail.com>
2024-03-09 23:04:14 +0300

    Update

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -3,5 +3,4 @@ const colors = ["синий", "черный", "зеленый", "красный"
 function createColorString() {
     return colors.filter(color => color === "черный" || color === "красный" || color === "желтый").join('-')
 }
-
 console.log(createColorString());
